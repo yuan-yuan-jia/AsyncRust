0b1bf35927761c21af6084b1bc5b36f850617e03
jyy <1305081045@qq.com>
2025-05-29 20:24:59 +0800

    basic future and contex

---
+++ ch02_01/src/main.rs
@@ -0,0 +1,41 @@
+use std::time::Duration;
+use tokio::time::sleep;
+use std::time::Instant;
+
+async fn prep_coffe_mug() {
+    sleep(Duration::from_millis(100)).await;
+    println!("Pouring milk...");
+    std::thread::sleep(Duration::from_secs(3));
+    println!("Milk poured");
+    println!("Putting instant coffee...");
+    std::thread::sleep(Duration::from_secs(3)); 
+    println!("Instant coffee put.")
+}
+
+async fn make_coffee() {
+    println!("boiling kettle...");
+    sleep(Duration::from_secs(10)).await;
+    println!("kettle boiled");
+    println!("pouring boiled water...");
+    std::thread::sleep(Duration::from_secs(3));
+    println!("boiled water poured");
+}
+async fn make_toast() {
+    println!("putting bread in toaster");
+    sleep(Duration::from_secs(10)).await;
+    println!("bread toasted.");
+    println!("buttering toasted bread...");
+    std::thread::sleep(Duration::from_secs(5));
+    println!("toasted bread buttered")
+}
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+async fn main() {
+    let start_time = Instant::now();
+    let coffee_mug_step = prep_coffe_mug();
+    let coffee_step = make_coffee();
+    let toast_step = make_toast();
+
+    tokio::join!(coffee_mug_step, coffee_step, toast_step);
+    let elapsed_time = start_time.elapsed();
+    println!("It took: {} seconds", elapsed_time.as_secs());
+}
